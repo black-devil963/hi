39a44326386da59e6d4b2160ee2718c4c2a94b0b
black-devil963 <89310171+black-devil963@users.noreply.github.com>
2023-03-03 15:47:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http=require("http")
 const port =process.env.PORT||8000;
 const server=http.createServer((req,res)=>{
-    res.end("response getting");
+    res.end(`Server is running on port ${port}`);
 })
 //npm install -g nodemon  => install live server for node 
 server.listen(port,"127.0.0.1",()=>{ //call back fn is required as server is asynchronus function
