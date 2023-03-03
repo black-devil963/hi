a2fc81059dc29779c59c2fc35652e2a0906ae431
black-devil963 <89310171+black-devil963@users.noreply.github.com>
2023-03-03 23:12:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const http=require("http") 
 const fs= require('fs')
 const url=require('url') 
-const prompt = msg => {
-    fs.writeSync(1, String(msg));
-    let s = '', buf = Buffer.alloc(1);
-    while(buf[0] - 10 && buf[0] - 13)
-      s += buf, fs.readSync(0, buf, 0, 1, 0);
-    return s.slice(1);
-  };
 //by defualt http ha url features but some features not supported so we use this
 const server=http.createServer((req,res)=>{ 
         
