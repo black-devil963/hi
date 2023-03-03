ae2b9641eb1cb02811b588d93166b2657947e02d
black-devil963 <89310171+black-devil963@users.noreply.github.com>
2023-03-03 19:57:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const http=require("http")
 const url=require('url')
 const fs=require("fs")
 const server=http.createServer((req,res)=>{ 
-    fs.readFile("index.js","utf-8",(err,data)=>
-{
-    console.log(data);
-})
+ const b=fs.readFileSync('Bye.txt');
+ res.end(b);   
 }) 
 server.listen(8000,"127.0.0.1",()=>{ 
     console.log("listening to port 8000") 
