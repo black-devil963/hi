34c331277624031852884bec4a1d0a35e1db6590
black-devil963 <89310171+black-devil963@users.noreply.github.com>
2023-03-03 19:59:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http=require("http")
 const url=require('url')
 const fs=require("fs")
 const server=http.createServer((req,res)=>{ 
- const b=fs.readFileSync('inddex.js');
+ const b=fs.readFileSync('index.js');
  res.end(b);   
 }) 
 server.listen(8000,"127.0.0.1",()=>{ 
